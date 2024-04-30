2fb8bd830b924df4e05ea14034fc583f345ce8db
WickerPrison <cobylax17@gmail.com>
2024-04-30 15:26:50 -0600

    added user and friend routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -8,11 +8,62 @@ router.get('/', async (req, res) => {
         res.json(users);
 })
 
+router.get('/:id', async (req, res) => {
+    const users = await User.findById(req.params.id).catch((err) =>{
+        res.json(err)
+    });
+    res.json(users);
+})
+
 router.post('/', async (req, res) => {
     const user = await User.create(req.body).catch((err) =>{
         res.json(err);
     });
     res.json(user);
+})
+
+router.put('/:id', async (req, res) => {
+    const users = await User.findByIdAndUpdate(req.params.id, 
+        {
+            username: req.body.username,
+            email: req.body.email
+        }, 
+        {new: true})
+        .catch((err) =>{
+        res.json(err)
+    });
+    res.json(users);
+})
+
+router.delete('/:id', async (req, res) =>{
+    const users = await User.findByIdAndDelete(req.params.id).catch((err) =>{
+        res.json(err)
+    });
+    res.json(users);
+})
+
+router.post('/:userId/friends/:friendId', async (req, res) => {
+    const users = await User.findByIdAndUpdate(req.params.userId, 
+        {
+            $set: {friends: req.params.friendId}
+        }, 
+        {new: true})
+        .catch((err) =>{
+        res.json(err)
+    });
+    res.json(users);
+})
+
+router.delete('/:userId/friends/:friendId', async (req, res) => {
+    const users = await User.findByIdAndUpdate(req.params.userId, 
+        {
+            $pull: {friends: req.params.friendId}
+        }, 
+        {new: true})
+        .catch((err) =>{
+        res.json(err)
+    });
+    res.json(users);
 })
 
 module.exports = router;
